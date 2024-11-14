8ebc72eb3e43cbee2796051bc520be6962fc2580
Asle Berge <asle.berge@gmail.com>
2024-11-14 12:50:32 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ import express from 'express'
 import payload from 'payload'
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3009
 
 const start = async (): Promise<void> => {
   await payload.init({
